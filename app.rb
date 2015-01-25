322c503542610a3d38d710ad107b0693fdffc49d
Po Ki Chui <pchui@flatworldknowledge.com>
2015-01-25 02:46:43 -0500

    refactored into init.rb Able to accept inputs and output in Formatter

--- app.rb
+++ app.rb
@@ -1,13 +1,24 @@
 require 'optparse'
 require 'ostruct'
 
+require_relative 'init'
+
 $options = OpenStruct.new
+$options.inputs = []
 
 optparser = OptionParser.new do |opts|
-  opts.banner = "Usage: ruby #{__FILE__} -i /path/to/input1,/path/to/input2... -o /path/to/output"
+  opts.banner = "Usage: ruby #{__FILE__} -s /path/to/space-delimited-input -p /path/to/pipe-delimited-input -c /path/to/comma-delimited-input -o /path/to/output"
 
-  opts.on('-i', '--input=', 'Input file path') do |i|
-    $options.input = i
+  opts.on('-s', '--space-input=', 'Input file path for space delimited file') do |i|
+    $options.inputs << { file: i, delimiter: ' ' }
+  end
+
+  opts.on('-c', '--comma-input=', 'Input file path for comma delimited file') do |i|
+    $options.inputs << { file: i, delimiter: ',' }
+  end
+
+  opts.on('-p', '--pipe-input=', 'Input file path for pipe delimited file') do |i|
+    $options.inputs << { file: i, delimiter: '|' }
   end
 
   opts.on('-o', '--output=', 'Output file path') do |o|
@@ -17,7 +28,17 @@ end
 
 optparser.parse!
 
-unless $options.input && $options.output
+unless $options.inputs
   puts optparser.help
-  exit(-1)
+  fail("Require at least one input types: -s | -c | -p")
 end
+
+unless $options.output
+  puts optparser.help
+  fail("Require an output file option: -o")
+end
+
+Priori::Formatter.new(
+  inputs: $options.inputs,
+  output: $options.output
+).format
--- lib/formatter.rb
+++ lib/formatter.rb
@@ -1,5 +1,20 @@
 module Priori
   class Formatter
+    def initialize(opts)
+      @inputs = opts[:inputs]
 
+      if opts[:output]
+        @output = opts[:output]
+        output_dir = File.dirname(@output)
+        FileUtil.mk_p(output_dir) unless File.exists? output_dir
+      end
+    end
+
+    def format
+      @inputs.each do |input|
+        input[:file]
+        input[:delimiter]
+      end
+    end
   end
 end
